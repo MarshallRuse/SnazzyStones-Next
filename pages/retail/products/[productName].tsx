02c473de3cfc484ce28c7ce16c44cd0dbbc27afd
Marshall Ruse <ruse.marshall@gmail.com>
2023-12-20 08:26:33 -0500

    Replaced Etsy product link with Share and Save link

--- pages/retail/products/[productName].tsx
+++ pages/retail/products/[productName].tsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from "react";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import { NextSeo } from "next-seo";
-import he from "he";
-import dayjs from "dayjs";
-import useCountry from "../../../utils/fetching/country";
-import Favorite from "@mui/icons-material/Favorite";
-import StarRateRounded from "@mui/icons-material/StarRateRounded";
-import { Skeleton } from "@mui/material";
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { NextSeo } from 'next-seo';
+import he from 'he';
+import dayjs from 'dayjs';
+import useCountry from '../../../utils/fetching/country';
+import Favorite from '@mui/icons-material/Favorite';
+import StarRateRounded from '@mui/icons-material/StarRateRounded';
+import { Skeleton } from '@mui/material';
 import {
     EmailIcon,
     EmailShareButton,
@@ -20,19 +20,19 @@ import {
     TwitterShareButton,
     WhatsappIcon,
     WhatsappShareButton,
-} from "react-share";
-import WiggleWrapper from "../../../components/WiggleWrapper";
-import formatProductTitleAsURL from "../../../utils/formatProductTitleAsURL";
-import { fetchCategoriesFromCache } from "../../../utils/fetching/categories/etsyCategories";
-import { fetchProductsFromCache } from "../../../utils/fetching/products/etsyProducts";
-import ImageGallery from "../../../components/ImageGallery";
-import ProductPageFallbackSkeleton from "../../../components/ProductPageFallbackSkeleton";
-import { ListingReview } from "../../../types/EtsyAPITypes";
-import { Product } from "../../../types/Types";
-import CTALink from "../../../components/CTAElements/CTALink";
-import { APIReviewsResponse } from "../../api/retail/products/reviews/[listing_id]";
+} from 'react-share';
+import WiggleWrapper from '../../../components/WiggleWrapper';
+import formatProductTitleAsURL from '../../../utils/formatProductTitleAsURL';
+import { fetchCategoriesFromCache } from '../../../utils/fetching/categories/etsyCategories';
+import { fetchProductsFromCache } from '../../../utils/fetching/products/etsyProducts';
+import ImageGallery from '../../../components/ImageGallery';
+import ProductPageFallbackSkeleton from '../../../components/ProductPageFallbackSkeleton';
+import { ListingReview } from '../../../types/EtsyAPITypes';
+import { Product } from '../../../types/Types';
+import CTALink from '../../../components/CTAElements/CTALink';
+import { APIReviewsResponse } from '../../api/retail/products/reviews/[listing_id]';
 
-const shareButtonStyle = "rounded-full focus:outline-none focus:ring focus:ring-bluegreen-500 focus:ring-offset-2";
+const shareButtonStyle = 'rounded-full focus:outline-none focus:ring focus:ring-bluegreen-500 focus:ring-offset-2';
 const shareButtonIconSize = 40;
 
 export interface ProductPageProps {
@@ -40,7 +40,7 @@ export interface ProductPageProps {
     category: string;
 }
 
-export default function ProductPage({ product = null, category = "" }: ProductPageProps) {
+export default function ProductPage({ product = null, category = '' }: ProductPageProps) {
     const { countryData, isLoading, isError } = useCountry();
     const router = useRouter();
     const productURL = `https://snazzystones.ca${router.asPath}`;
@@ -66,42 +66,45 @@ export default function ProductPage({ product = null, category = "" }: ProductPa
     return (
         <>
             <NextSeo
-                title={`${product.title.split("|")[0].trim()} | Snazzy Stones`}
-                description={he.decode(product.description.split("\n")[0].trim())}
+                title={`${product.title.split('|')[0].trim()} | Snazzy Stones`}
+                description={he.decode(product.description.split('\n')[0].trim())}
                 canonical={product.url}
                 openGraph={{
                     url: productURL,
-                    title: `${product.title.split("|")[0].trim()} | Snazzy Stones`,
-                    description: he.decode(product.description.split("\n")[0].trim()),
+                    title: `${product.title.split('|')[0].trim()} | Snazzy Stones`,
+                    description: he.decode(product.description.split('\n')[0].trim()),
                     images: product.images.map((img, ind) => ({
                         url: img.url_fullxfull,
                         width: 442,
                         height: 442,
-                        alt: `Product listing image ${ind + 1} for ${product.title.split("|")[0].trim()}`,
-                        type: "image/jpeg",
+                        alt: `Product listing image ${ind + 1} for ${product.title.split('|')[0].trim()}`,
+                        type: 'image/jpeg',
                     })),
-                    site_name: "SnazzyStones",
+                    site_name: 'SnazzyStones',
                 }}
                 twitter={{
-                    cardType: "summary",
+                    cardType: 'summary',
                 }}
             />
             <section className='grid md:grid-cols-2 md:grid-flow-row auto-rows-max gap-10 py-16 md:max-w-screen-lg justify-center mx-auto px-4'>
-                <ImageGallery images={product.images} productTitle={product.title} />
+                <ImageGallery
+                    images={product.images}
+                    productTitle={product.title}
+                />
                 <div className='flex flex-col row-span-2 text-sm text-slate-500 max-w-sm md:max-w-lg pt-2'>
                     <nav className='flex flex-nowrap'>
                         <Link href='/'>
                             <a className='text-bluegreen-500 navItem max-w-max inline-flex mx-1'>Home</a>
-                        </Link>{" "}
+                        </Link>{' '}
                         {category && (
                             <>
                                 /
-                                <Link href={`/retail/categories/${category?.replace(" ", "_")}`}>
+                                <Link href={`/retail/categories/${category?.replace(' ', '_')}`}>
                                     <a className='text-bluegreen-500 navItem max-w-max inline-flex mx-1'>{category}</a>
                                 </Link>
                             </>
                         )}
-                        {product && <>/ {product.title.split("|")[0].trim()}</>}
+                        {product && <>/ {product.title.split('|')[0].trim()}</>}
                     </nav>
                     <div className='flex flex-col md:flex-row items-start md:items-center md:gap-4 mb-12 md:mb-0 text-blueyonder-500'>
                         <h1 className='text-2xl mt-4 font-semibold mb-4 md:mb-auto'>{product.title}</h1>
@@ -137,15 +140,18 @@ export default function ProductPage({ product = null, category = "" }: ProductPa
                     <div className='max-h-56 mt-4 overflow-y-auto subtleScrollbar'>
                         {product &&
                             product.description
-                                .split("\n")
-                                .filter((line) => line !== "")
+                                .split('\n')
+                                .filter((line) => line !== '')
                                 .map((line) => he.decode(line))
                                 .map((line, ind) => (
-                                    <p key={`description-${ind}`} className={`${ind === 0 ? "mt-0" : ""} mb-0 text-lg`}>
+                                    <p
+                                        key={`description-${ind}`}
+                                        className={`${ind === 0 ? 'mt-0' : ''} mb-0 text-lg`}
+                                    >
                                         {line
-                                            .split(" ")
+                                            .split(' ')
                                             .map((word, subInd) =>
-                                                word.substring(0, 8) === "https://" ? (
+                                                word.substring(0, 8) === 'https://' ? (
                                                     <a
                                                         key={`description-link-${subInd}`}
                                                         href={word}
@@ -160,7 +166,7 @@ export default function ProductPage({ product = null, category = "" }: ProductPa
                                                 )
                                             )
                                             .reduce((accu, elem) => {
-                                                return accu === null ? [elem] : [...accu, " ", elem];
+                                                return accu === null ? [elem] : [...accu, ' ', elem];
                                             }, null)}
                                     </p>
                                 ))}
@@ -170,12 +176,16 @@ export default function ProductPage({ product = null, category = "" }: ProductPa
                         <span className='text-bluegreen-500'> {product.quantity} in stock</span>
                     </p>
                     <div className='flex flex-col md:flex-row items-center gap-4'>
-                        <CTALink href={product.url} target='_blank' rel='noreferrer'>
+                        <CTALink
+                            href={`https://snazzystonesjewelry.etsy.com/listing/${product.listing_id}`}
+                            target='_blank'
+                            rel='noreferrer'
+                        >
                             Purchase on Etsy
                         </CTALink>
                         {product.num_favorers > 0 && (
                             <div className='text-bluegreen-500 font-medium'>
-                                <Favorite /> {product.num_favorers} {product.num_favorers > 1 ? "people" : "person"}{" "}
+                                <Favorite /> {product.num_favorers} {product.num_favorers > 1 ? 'people' : 'person'}{' '}
                                 favorited this item!
                             </div>
                         )}
@@ -185,8 +195,14 @@ export default function ProductPage({ product = null, category = "" }: ProductPa
                     <h3 className='text-bluegreen-500 mb-4'>Share the Snazziness!</h3>
                     <div className='flex items-start gap-4'>
                         <WiggleWrapper>
-                            <FacebookShareButton url={productURL} className={shareButtonStyle}>
-                                <FacebookIcon size={shareButtonIconSize} round />
+                            <FacebookShareButton
+                                url={productURL}
+                                className={shareButtonStyle}
+                            >
+                                <FacebookIcon
+                                    size={shareButtonIconSize}
+                                    round
+                                />
                             </FacebookShareButton>
                         </WiggleWrapper>
                         <WiggleWrapper>
@@ -195,22 +211,43 @@ export default function ProductPage({ product = null, category = "" }: ProductPa
                                 className={shareButtonStyle}
                                 appId={product.facebookAppId}
                             >
-                                <FacebookMessengerIcon size={shareButtonIconSize} round />
+                                <FacebookMessengerIcon
+                                    size={shareButtonIconSize}
+                                    round
+                                />
                             </FacebookMessengerShareButton>
                         </WiggleWrapper>
                         <WiggleWrapper>
-                            <TwitterShareButton url={productURL} className={shareButtonStyle}>
-                                <TwitterIcon size={shareButtonIconSize} round />
+                            <TwitterShareButton
+                                url={productURL}
+                                className={shareButtonStyle}
+                            >
+                                <TwitterIcon
+                                    size={shareButtonIconSize}
+                                    round
+                                />
                             </TwitterShareButton>
                         </WiggleWrapper>
                         <WiggleWrapper>
-                            <WhatsappShareButton url={productURL} className={shareButtonStyle}>
-                                <WhatsappIcon size={shareButtonIconSize} round />
+                            <WhatsappShareButton
+                                url={productURL}
+                                className={shareButtonStyle}
+                            >
+                                <WhatsappIcon
+                                    size={shareButtonIconSize}
+                                    round
+                                />
                             </WhatsappShareButton>
                         </WiggleWrapper>
                         <WiggleWrapper>
-                            <EmailShareButton url={productURL} className={shareButtonStyle}>
-                                <EmailIcon size={shareButtonIconSize} round />
+                            <EmailShareButton
+                                url={productURL}
+                                className={shareButtonStyle}
+                            >
+                                <EmailIcon
+                                    size={shareButtonIconSize}
+                                    round
+                                />
                             </EmailShareButton>
                         </WiggleWrapper>
                     </div>
@@ -219,11 +256,11 @@ export default function ProductPage({ product = null, category = "" }: ProductPa
 
             <section className='px-4 py-16 md:max-w-screen-lg mx-auto  border-t border-slate-100'>
                 <h2 className='text-blueyonder-500'>
-                    Reviews{" "}
+                    Reviews{' '}
                     {reviews.length > 0 ? (
                         <span className='text-blueyonder-400 text-2xl inline-flex items-center'>
-                            ({reviews.length}, Average{" "}
-                            {(reviews.reduce((acc, curr) => acc + curr.rating, 0) / reviews.length).toFixed(1)}{" "}
+                            ({reviews.length}, Average{' '}
+                            {(reviews.reduce((acc, curr) => acc + curr.rating, 0) / reviews.length).toFixed(1)}{' '}
                             <StarRateRounded />)
                         </span>
                     ) : (
@@ -233,7 +270,10 @@ export default function ProductPage({ product = null, category = "" }: ProductPa
                 {reviews
                     ?.sort((a, b) => b.created_timestamp - a.created_timestamp)
                     .map((review, ind) => (
-                        <div key={`review-${ind}`} className='py-4'>
+                        <div
+                            key={`review-${ind}`}
+                            className='py-4'
+                        >
                             <div className='flex items-end gap-4'>
                                 <div className='text-bluegreen-500'>
                                     {[...Array(review.rating)].map((e, i) => (
@@ -241,7 +281,7 @@ export default function ProductPage({ product = null, category = "" }: ProductPa
                                     ))}
                                 </div>
                                 <div className='text-blueyonder-400'>
-                                    {dayjs.unix(review.created_timestamp).format("MMMM DD, YYYY")}
+                                    {dayjs.unix(review.created_timestamp).format('MMMM DD, YYYY')}
                                 </div>
                             </div>
                             <div className='flex flex-col md:flex-row items-center justify-between gap-2 md:gap-4'>
@@ -272,12 +312,12 @@ export async function getStaticPaths() {
     return {
         paths: activeShopListingsFormatted.map((listing) => ({
             params: {
-                productName: listing.title.includes("|")
+                productName: listing.title.includes('|')
                     ? formatProductTitleAsURL(listing.title)
                     : listing.listing_id.toString(),
             },
         })),
-        fallback: "blocking",
+        fallback: 'blocking',
     };
 }
 
@@ -287,7 +327,7 @@ export async function getStaticProps(context) {
     const products = await fetchProductsFromCache();
 
     const productToGet = products.find((prod) => {
-        if (prod.title.includes("|")) {
+        if (prod.title.includes('|')) {
             return formatProductTitleAsURL(prod.title) === params.productName;
         } else {
             return prod.listing_id === params.productName;
